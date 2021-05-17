41145e126b87efdad43767f365acab3f1c7c65ac
smriad <skmdriad@gmail.com>
2021-05-17 23:54:06 +0800

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDF-Eq7UQzzghG5WlhVthxgJQde4Co5tmk",
+    authDomain: "ema-john-ecom-739bf.firebaseapp.com",
+    projectId: "ema-john-ecom-739bf",
+    storageBucket: "ema-john-ecom-739bf.appspot.com",
+    messagingSenderId: "540882563441",
+    appId: "1:540882563441:web:2efd7f50f30eba25cd86d0"
+  };
+ 
+  export default firebaseConfig;
